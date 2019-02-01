6fa915e68f4f56503513f1fe89f56ba4f4379c27
Racing Race <got26days@gmail.com>
2019-02-01 14:39:07 +0300

    faq

--- resources/views/pages/faq.blade.php
+++ resources/views/pages/faq.blade.php
@@ -16,10 +16,10 @@
                 @foreach($faqs as $faq)
                 <div class="faq__item js-accordion-item">
                     <div class="faq__item-header">
-                        <h3 class="faq__item-title">{{ $faq->question }}</h3>
+                        <h3 class="faq__item-title" style="white-space: pre-line;">{{ $faq->question }}</h3>
                         <button class="btn btn--green faq__item-btn js-accordion-btn">Ответ</button>
                     </div>
-                    <div class="faq__item-main js-accordion-hide">
+                    <div class="faq__item-main js-accordion-hide" style="white-space: pre-line;">
                         {{ $faq->answer }}
                     </div>
                 </div>
