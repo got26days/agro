1eebb128c7c702983882cdeffefb15e4355cc8bb
Racing Race <got26days@gmail.com>
2019-02-08 15:42:03 +0300

    metric

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -164,5 +164,21 @@
             map.geoObjects.add(placemark);
         };        
     </script><!--карты-->
+
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript" >
+        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+    
+        ym(52287526, "init", {
+            id:52287526,
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true
+        });
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/52287526" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
 </body>
 </html>
