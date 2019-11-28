6344f7e051b9cda7d318483b0cc5aa5ada011711
Sergey <got26days@gmail.com>
2019-11-28 09:15:10 +0300

    Change data

--- app/Http/Controllers/DronController.php
+++ app/Http/Controllers/DronController.php
@@ -17,7 +17,7 @@ class DronController extends Controller
         $domain = Buh::latest()->first();
 
         if(!$domain){
-            return redirect('https://agrodohod.ru/404');
+            return redirect('http://agrodohod.ru/404');
         }
 
         $teams = Team::get();
@@ -39,7 +39,7 @@ class DronController extends Controller
         $domain = Pr::latest()->first();
 
         if(!$domain){
-            return redirect('https://agrodohod.ru/404');
+            return redirect('http://agrodohod.ru/404');
         }
 
         $teams = Team::get();
